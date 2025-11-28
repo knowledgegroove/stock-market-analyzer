ef14a752d149e2c50e5d5360c97b0dbf97219663
knowledgegroove <knowledgegroove1980@gmail.com>
2025-11-27 21:10:42 -0800

    Disable basePath and assetPrefix in config
    Comment out basePath, assetPrefix, and reactStrictMode.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 // next.config.mjs
 const nextConfig = {
-  basePath: '/stock-market-analyzer',
-  assetPrefix: '/stock-market-analyzer',
-  reactStrictMode: true,
+  // basePath: '/stock-market-analyzer',
+  // assetPrefix: '/stock-market-analyzer',
+  // reactStrictMode: true,
 };
 
 export default nextConfig;
